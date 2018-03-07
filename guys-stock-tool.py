cc1d11e34b611d6c18eed32abe9cc5e5c607a566
corey-guy <corey.drees@gmail.com>
2018-03-07 17:47:35 -0500

    Initial message created, guy print helper module created

---
+++ guys-stock-tool.py
@@ -0,0 +1,5 @@
+import guy_print
+
+print "Welcome to Guys Stock Tool! A Stock Portfolio maintainence program and information giver...designed by Guy, for Guy!"
+guy_print.print_with_dashes("Do you want to hear a joke? (Y/N)")
+
