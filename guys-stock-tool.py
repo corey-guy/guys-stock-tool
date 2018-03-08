fb05316940cd3883d741922ffcecb006b0fb9610
corey-guy <corey.drees@gmail.com>
2018-03-07 19:11:48 -0500

    refactored intro

--- guys-stock-tool.py
+++ guys-stock-tool.py
@@ -1,8 +1,10 @@
 import utilities.guy_print as guy_print
 
 def intro():
+    print "Welcome to Guys Stock Tool! A Stock Portfolio maintainence program and information giver...designed by Guy, for Guy!"
+  
+def load_portfolio():
    option_is_valid = False
-   print "Welcome to Guys Stock Tool! A Stock Portfolio maintainence program and information giver...designed by Guy, for Guy!"
    guy_print.print_with_dashes("What do you want to do?\n1.Start a New Portfolio\n2.Load Existing Portfolio")
    while(option_is_valid != True):
       input = raw_input()
@@ -14,6 +16,8 @@ def intro():
          option_is_valid = True
       else:
          print "Sorry, try choosing an option again"
-      
+    
+   
 intro()
+portfolio = load_portfolio()
 
