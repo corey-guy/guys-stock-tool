abb58e9ad60c3f1f4e681a4137cd720d76d99c92
corey-guy <corey.drees@gmail.com>
2018-03-07 21:04:23 -0500

    Added remove stock function

--- portfolio.py
+++ portfolio.py
@@ -13,3 +13,13 @@ class Portfolio:
    def show_portfolio(self):
       for x in self.stocks:
          print str(self.stocks[x]) + " share(s) of " + x
+
+   def get_stock_amount(self, stock_abbr):
+      return self.stocks[stock_abbr]
+   def remove_stock(self, stock_abbr, amount):
+      try:
+         self.stocks[stock_abbr] -= amount
+         return 0
+      except KeyError, e:
+         return -1
+
